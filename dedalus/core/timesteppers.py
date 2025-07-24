ec34c5299a8ae3803c2bf39fef7f99bc3d49d604
Calum Skene <c.s.skene@leeds.ac.uk>
2025-07-24 11:52:24 +0100

    Simplify RHS vjps with expression list

--- dedalus/core/timesteppers.py
+++ dedalus/core/timesteppers.py
@@ -7,6 +7,7 @@ from scipy.linalg import blas
 from .system import CoeffSystem
 from ..tools.array import apply_sparse
 from .field import Field
+from .future import ExpressionList
 import uuid
 
 # Public interface
@@ -99,6 +100,7 @@ class MultistepIMEX:
                 # corresponding name
                 field_adj.name = 'Y_adj%s' % field.name
             self.Y_fields.append(field_adj)
+        self.F_expression_list = ExpressionList([eqn['F'] for eqn in solver.problem.equations if not isinstance(eqn['F'], Field)])
         # TODO: How to handle checkpointing?
         self.timestep_history = []
         self.cotangents = {}
@@ -289,7 +291,7 @@ class MultistepIMEX:
         sum_len = np.min([len(c), solver.stop_iteration-self._iteration])
         # Cache VJPs where solver.state does not change
         # For now, make an id for each equation
-        ids = [uuid.uuid4() for eqn in solver.problem.equations]
+        id = uuid.uuid4()
         # Calculate linearised F for all steps
         for j in range(sum_len):
             for sp in subproblems:
@@ -300,11 +302,7 @@ class MultistepIMEX:
                     self.cotangents[field].preset_layout('c')
                     self.cotangents[field].data.fill(0)
             # Loop over equations and accumulate cotangents
-            for eqn_num, eqn in enumerate(solver.problem.equations):
-                # TODO: Fix this when fields have vjp
-                if not isinstance(eqn['F'], Field):
-                    # Calculate vjp
-                    _, self.cotangents = eqn['F'].evaluate_vjp(self.cotangents, id=ids[eqn_num], force=True)
+            _, self.cotangents = self.F_expression_list.evaluate_vjp(self.cotangents, id=id, force=True)
             # Require coeff space before gathers
             for field in self.dFdxH_Y:
                 field.require_coeff_space()
@@ -710,6 +708,7 @@ class RungeKuttaIMEX:
                 # corresponding name
                 field_adj.name = 'Y_adj%s' % field.name
             self.Y_fields.append(field_adj)
+        self.F_expression_list = ExpressionList([eqn['F'] for eqn in solver.problem.equations if not isinstance(eqn['F'], Field)])
         self.cotangents = {}
         for i, eqn in enumerate(solver.problem.equations):
             # TODO: Fix this when fields have vjp
@@ -911,8 +910,7 @@ class RungeKuttaIMEX:
                 sp.scatter_inputs(XStages[i-1].get_subdata(sp), solver.state)
                 np.copyto(Y[i-1].get_subdata(sp),spX)
             # Cache VJPs where solver.state does not change
-            # For now, make an id for each equation
-            ids = [uuid.uuid4() for eqn in solver.problem.equations]
+            id = uuid.uuid4()
             # Note, similar code here to MultistepIMEX
             for j in range(i,self.stages+1):
                 F[j-1].data.fill(0)
@@ -926,11 +924,7 @@ class RungeKuttaIMEX:
                         self.cotangents[field].preset_layout('c')
                         self.cotangents[field].data.fill(0)
                 # Loop over equations and accumulate cotangents
-                for eqn_num, eqn in enumerate(solver.problem.equations):
-                    # TODO: Fix this when fields have vjp
-                    if not isinstance(eqn['F'], Field):
-                        # Calculate vjp
-                        _, self.cotangents = eqn['F'].evaluate_vjp(self.cotangents, id=ids[eqn_num], force=True)
+                _, self.cotangents = self.F_expression_list.evaluate_vjp(self.cotangents, id=id, force=True)
                 # Require coeff space before gathers
                 for field in self.dFdxH_Y:
                     field.require_coeff_space()
